afd561aef721c73f6dbeda5ef8752f3323a72538
Muhammad Rehan Alam <rehan@beebolt.com>
2025-12-09 11:56:27 +0500

    adds shadcn uI components

---
+++ src/components/ui/heading.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react'
+import { cva } from 'class-variance-authority'
+import type { VariantProps } from 'class-variance-authority'
+import { cn } from '@/lib/utils'
+
+const headingVariants = cva('font-semibold tracking-tight', {
+  variants: {
+    level: {
+      1: 'text-5xl',
+      2: 'text-4xl',
+      3: 'text-3xl',
+      4: 'text-2xl',
+      5: 'text-xl',
+      6: 'text-lg',
+    },
+    variant: {
+      primary: 'text-foreground',
+      secondary: 'text-muted-foreground',
+    },
+  },
+  defaultVariants: {
+    level: 1,
+    variant: 'primary',
+  },
+})
+
+export interface HeadingProps
+  extends
+    React.HTMLAttributes<HTMLHeadingElement>,
+    VariantProps<typeof headingVariants> {
+  level?: 1 | 2 | 3 | 4 | 5 | 6
+}
+
+const Heading = React.forwardRef<HTMLHeadingElement, HeadingProps>(
+  ({ className, level = 1, variant, ...props }, ref) => {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
+    const Component = `h${level}` as 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
+
+    return (
+      <Component
+        ref={ref}
+        className={cn(headingVariants({ level, variant, className }))}
+        {...props}
+      />
+    )
+  },
+)
+
+Heading.displayName = 'Heading'
+
+export { Heading, headingVariants }
